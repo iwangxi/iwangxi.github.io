dd15bcd973d337b920043395e6376f34d1b10a9a
iwangxi <13010998@qq.com>
2025-05-22 14:19:24 +0800

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:iwangxi/iwangxi.github.io.git main
+git push -f git@github.com:iwangxi/iwangxi.github.io.git build
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
